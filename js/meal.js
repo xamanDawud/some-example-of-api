347536ae664b8f47d3826d38e90bbb9653cb350d
xamanDawud <ahmeddaudking100@gmail.com>
2022-09-01 05:02:04 +0600

    Meal Project some information updated

--- js/meal.js
+++ js/meal.js
@@ -10,7 +10,7 @@ let getdata = (data) => {
     for (let food of data) {
         let div = document.createElement("div");
         div.innerHTML = `
-        <div class="card">
+        <div class="card" onclick="loadFoodDetails(${food.idMeal})">
         <img class="card-img-top" src="${
           food.strMealThumb
         }" alt="Card image cap" />
@@ -27,7 +27,6 @@ let getdata = (data) => {
         `;
         div.classList.add("card-deck");
         cardSection.appendChild(div);
-        console.log(food.strMeal);
     }
 };
 
@@ -39,3 +38,36 @@ document.getElementById("btn").addEventListener("click", function() {
     }
     foodFetch(searchWord());
 });
+
+let loadFoodDetails = (idMeal) => {
+    displayDetailsWithIdmeal(idMeal);
+};
+
+let displayDetailsWithIdmeal = (id) => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then((res) => res.json())
+        .then((info) => displayDetailsSet(info.meals[0]));
+};
+
+let displayDetailsSet = (info) => {
+    let section = document.getElementById("display-details");
+    section.innerHTML = "";
+    let div = document.createElement("div");
+    div.innerHTML = `
+        <img class="card-img-top" src="${
+          info.strMealThumb
+        }" alt="Card image cap" />
+        <div class="card-body">
+            <h5 class="card-title">${info.strMeal}</h5>
+            <p class="card-text">
+                ${info.strInstructions.slice(0, 200)}
+            </p>
+            <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+        </div>
+    </div>
+        `;
+    div.classList.add("card-deck");
+    section.appendChild(div);
+};
