9e9122b02bf3e5c983a45587e6b54f6caae44cdb
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-31 15:51:57 +0600

    country details and falg shown using by countries api

--- js/countries-res.js
+++ js/countries-res.js
@@ -1,6 +1,6 @@
 fetch("https://restcountries.com/v3.1/all")
     .then((res) => res.json())
-    .then((data) => getData(data));
+    .then((data) => getData(data, data));
 
 let getData = (resourse) => {
     let section = document.getElementById("section");
@@ -10,9 +10,25 @@ let getData = (resourse) => {
         <p>Country Name: ${res.name.common}</p>
         <p>Title: ${res.name.official}</p>
         <p>Population: ${res.population}</p>
-        
+        <button onclick=getCode('${res.cca2}')>Details</button>
         `;
         div.classList.add("graphic");
         section.appendChild(div);
     });
+};
+
+function getCode(code) {
+    let url = `https://restcountries.com/v3.1/alpha/${code}`;
+    fetch(url)
+        .then((country_details) => country_details.json())
+        .then((data) => showFlag(data));
+}
+
+let showFlag = (flag) => {
+    let section = document.getElementById("show-flag");
+    for (let info of flag) {
+        let img = document.getElementById("img");
+        img.setAttribute("src", `${info.flags.png}`);
+        console.log(info.flags.png);
+    }
 };
